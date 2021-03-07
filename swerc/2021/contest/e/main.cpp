09df298e3533044dd9fcb7423683f5ec18850fdb
Stefano Huber <stefano.huber@gmail.com>
2021-03-07 18:55:23 +0100

    Add problem e swerc 2021, easy greedy

---
+++ swerc/2021/contest/e/main.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include <climits>
+#include <cmath>
+#include <unordered_set>
+
+using namespace std;
+
+// #define TESTING
+
+#ifdef TESTING 
+#define DEBUG cout << "====TESTING====\n" 
+#define VALUE(x) cout << "The value of " << #x << " is " << x << endl 
+#define PAIR(x) cout << x.first << "\t" << x.second << endl
+#define VECT(x) for (int u = 0; u<x.size(); u++) cout << x[u] << endl; cout << endl
+#define COUT(x) cout << x 
+
+#else 
+#define DEBUG 
+#define VALUE(x) 
+#define PAIR(x) 
+#define VECT(x)
+#define COUT(x)
+#endif 
+
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<vector<int>> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<bool> vb;
+#define FOR(i,a,b) for (int i=(a); i<(b); i++)
+#define MODULUS 10000
+#define DELTA 0.0000001
+
+
+
+
+int main(){
+    int n;
+    cin >> n;
+    int res = INT32_MAX;
+    FOR(i,0,n){
+        int a,b;
+        cin >> a >> b;
+        res = min(res, b / a);
+    }
+    cout << res << endl;
+}
