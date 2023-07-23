6ea135b22881adc4db89ec24efe0f55bdfd6fd26
Stefano <stefano.huber@gmail.com>
2023-07-23 15:59:04 +0200

    Reply coding challenge 2021 and 2022 submit.sh script

---
+++ reply/coding/2021/submit.sh
@@ -0,0 +1,7 @@
+g++ main.cpp -O2
+./a.out < input/data_scenarios_a_example.in > output/a.txt
+./a.out < input/data_scenarios_b_mumbai.in > output/b.txt
+./a.out < input/data_scenarios_c_metropolis.in > output/c.txt
+./a.out < input/data_scenarios_d_polynesia.in > output/d.txt
+./a.out < input/data_scenarios_e_sanfrancisco.in > output/e.txt
+./a.out < input/data_scenarios_f_tokyo.in > output/f.txt
---
+++ reply/coding/2022/submit.sh
@@ -0,0 +1,13 @@
+g++ main.cpp -O2
+./a.out < input/00.txt > output/00.txt 
+echo 0
+./a.out < input/01.txt > output/01.txt 
+echo 1
+./a.out < input/02.txt > output/02.txt 
+echo 2
+# ./a.out < input/03.txt > output/03.txt 
+echo 3
+./a.out < input/04.txt > output/04.txt 
+echo 4
+./a.out < input/05.txt > output/05.txt 
+echo 5
